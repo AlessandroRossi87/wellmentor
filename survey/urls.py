106e7dcf18bda7e33eebd814c6e8459ae298e9ed
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2023-11-23 19:07:51 +0000

    adding rout navbar

--- survey/urls.py
+++ survey/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< Updated upstream
     path('', views.SurveyView.as_view(), name='survey'),
     path('/results', views.SurveyResultsView.as_view(), name='results'),
     path('/about-us', views.AboutUsView.as_view(), name='about_us'),
+=======
+    path('', views.HomePageView.as_view(), name='home'),
+    path('survey', views.SurveyView.as_view(), name='survey'),
+    path('results', views.SurveyResultsView.as_view(), name='results'),
+    path('about-us', views.AboutUsView.as_view(), name='about_us'),
+    path('accounts/profile/', views.HomePageView.as_view(), name='profile'), 
+>>>>>>> Stashed changes
 ]
